9c206125b5b6432bc03ecceb7c02837bf6723f89
Anthony <perezant121@gmail.com>
2022-08-29 22:59:27 -0400

    added icons for weather and wind record

--- assets/script.js
+++ assets/script.js
@@ -30,18 +30,33 @@ var getWeather = function(city) {
 };
 
 var displayWeather = function (data, city) {
+    console.log(data);
     var h2El = document.createElement('h2');
     var tempEl = document.createElement('p');
+    var windEL = document.createElement('p')
+    var humidityEL = document.createElement('p')
+    var uvEL = document.createElement('p')
+    var icon = data.current.weather[0].icon;
+    var imgEL = document.createElement('img');
     h2El.textContent = city.name;
     tempEl.textContent = 'TEMP: ' + data.current.temp + "°F";
+    windEL.textContent = 'WIND: ' + data.current.wind_speed +'MPH';
+
+    imgEL.width = 90;
+    imgEL.height = 90;
+    imgEL.alt= icon;
+    imgEL.src= `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
     jumbotronEl.appendChild(h2El);
+    jumbotronEl.appendChild(imgEL);
     jumbotronEl.appendChild(tempEl);
+    jumbotronEl.appendChild(windEL);
     
     var fiveDays = data.daily.slice(1,6);
     console.log(fiveDays);
-
     fiveDaysEl.innerHTML = null;
     for (var day of fiveDays) {
+        var icon = day.weather[0].icon;
         var date = new Date(day.dt * 1000).toLocaleDateString();
         var temp = day.temp.day;
         var divOne = document.createElement('div');
@@ -53,13 +68,20 @@ var displayWeather = function (data, city) {
         h5El.className = "card-title";
         var tempEl = document.createElement('p');
         tempEl.className = "card-text";
+        var imgEL = document.createElement('img'); 
 
         h5El.textContent = date;
         tempEl.textContent = "TEMP: " + temp + "°F";
+        
+        imgEL.width = 45;
+        imgEL.height = 45;
+        imgEL.alt = icon;
+        imgEL.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
         fiveDaysEl.appendChild(divOne);
         divOne.appendChild(divTwo);
         divTwo.appendChild(h5El);
+        divTwo.appendChild(imgEL);
         divTwo.appendChild(tempEl);
         
     }
