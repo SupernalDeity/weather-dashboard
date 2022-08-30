fa683e9b8950ee3ee87bab8bb425fadf7dc7f266
Anthony <perezant121@gmail.com>
2022-08-29 21:45:09 -0400

    used locale storage to present previously searched for cities

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ var appid = '485bbc753e29e9770f09ca55c32c6d79';
 var citySearch = document.querySelector('#citySearch');
 var jumbotronEl = document.querySelector('#jumbotron');
 var searchBtn = document.querySelector('#search');
+var searchedCitiesEl = document.querySelector('#searchedCities');
 
 var toJSON =function(response) {
     return response.json();
@@ -11,8 +12,9 @@ var getLocation = function (locations) {
         var city = locations[0];
         console.log('LAT', city.lat);
         console.log('LON', city.lon);
-        
+
         saveLocaleStorage(city.name);
+        displaySearchedCities();
         getWeather(city);
 };
 
@@ -33,6 +35,20 @@ var displayWeather = function (data, city) {
     tempEl.textContent = 'TEMP: ' + data.current.temp;
     jumbotronEl.appendChild(h2El);
     jumbotronEl.appendChild(tempEl);
+};
+
+var displaySearchedCities = function() {
+    var cities = JSON.parse(localStorage.getItem('SearchedCities')) || [];
+    searchedCitiesEl.innerHTML = null;
+    for (var city of cities) {
+        var cityLiEl = document.createElement('li');
+        cityLiEl.className = "list-group-item";
+        var cityButtonEl = document.createElement('button');
+        cityButtonEl.textContent = city;
+        cityButtonEl.className = "btn btn-success w-100 text-center";
+        cityLiEl.appendChild(cityButtonEl);
+        searchedCitiesEl.appendChild(cityLiEl);
+    }
 };
 
 var searchCity = function(event) {
