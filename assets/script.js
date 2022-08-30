0eb0a53e5549a59f86a12ec3533caa6cb3007572
Anthony <perezant121@gmail.com>
2022-08-29 20:47:57 -0400

    cleaned up functions and set way to display city and current weather

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,18 @@
 var appid = '485bbc753e29e9770f09ca55c32c6d79';
 var q = document.querySelector('#q');
+var jumbotronEl = document.querySelector('#jumbotron');
 
 q = "Charlotte"
 
 var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${appid}`;
 
+var toJSON =function(response) {
+    return response.json();
+};
 
 function getLocation() {
     fetch(geoURL)
-        .then(function (response) {
-            return response.json();
-        })
+        .then(toJSON)
         .then(function (locations) {
             var city = locations[0];
             console.log('LAT', city.lat);
@@ -24,13 +26,22 @@ var getWeather = function(city) {
     var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${city.lat}&lon=${city.lon}&appid=${appid}&units=imperial&exclude=hourly,minutely`;
 
     fetch(oneCall)
-        .then(function (response) {
-            return response.json();
-        })
+        .then(toJSON)
         .then(function(data){
-            console.log(data);
+            displayWeather(data, city);
         })
-
 };
 
-getLocation();
+var displayWeather = function (data, city) {
+    var h2El = document.createElement('h2');
+    var tempEl = document.createElement('p');
+    h2El.textContent = city.name;
+    tempEl.textContent = 'TEMP: ' + data.current.temp;
+    jumbotronEl.appendChild(h2El);
+    jumbotronEl.appendChild(tempEl);
+};
+
+
+fetch(geoURL)
+    .then(toJSON)
+    .then(getLocation);
