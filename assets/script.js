a00a9b29134902ef987da00522d2ea3db92d7a0f
Anthony <perezant121@gmail.com>
2022-08-29 22:36:40 -0400

    used javascript to creat the 5 days temp section and display info

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ var citySearch = document.querySelector('#citySearch');
 var jumbotronEl = document.querySelector('#jumbotron');
 var searchBtn = document.querySelector('#search');
 var searchedCitiesEl = document.querySelector('#searchedCities');
+var fiveDaysEl = document.querySelector('#fiveDays');
 
 var toJSON =function(response) {
     return response.json();
@@ -32,9 +33,36 @@ var displayWeather = function (data, city) {
     var h2El = document.createElement('h2');
     var tempEl = document.createElement('p');
     h2El.textContent = city.name;
-    tempEl.textContent = 'TEMP: ' + data.current.temp;
+    tempEl.textContent = 'TEMP: ' + data.current.temp + "°F";
     jumbotronEl.appendChild(h2El);
     jumbotronEl.appendChild(tempEl);
+    
+    var fiveDays = data.daily.slice(1,6);
+    console.log(fiveDays);
+
+    fiveDaysEl.innerHTML = null;
+    for (var day of fiveDays) {
+        var date = new Date(day.dt * 1000).toLocaleDateString();
+        var temp = day.temp.day;
+        var divOne = document.createElement('div');
+        divOne.className = "card col-12 col-md-2 mx-2";
+        divOne.style.width = '18rem';
+        var divTwo = document.createElement('div');
+        divTwo.className = "card-body";
+        var h5El = document.createElement('h5');
+        h5El.className = "card-title";
+        var tempEl = document.createElement('p');
+        tempEl.className = "card-text";
+
+        h5El.textContent = date;
+        tempEl.textContent = "TEMP: " + temp + "°F";
+
+        fiveDaysEl.appendChild(divOne);
+        divOne.appendChild(divTwo);
+        divTwo.appendChild(h5El);
+        divTwo.appendChild(tempEl);
+        
+    }
 };
 
 var displaySearchedCities = function() {
@@ -70,6 +98,7 @@ var saveLocaleStorage = function(city) {
 
 var oldSearches = function(event) {
     event.preventDefault();
+    jumbotronEl.innerHTML = null;
     if (event.target.matches('button')) {
         var citySearch = event.target.textContent;
         var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${citySearch}&appid=${appid}`;
